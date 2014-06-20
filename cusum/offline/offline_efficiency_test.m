51f5481223dcebfd0e75a0c929548a49640a7b5d
chilleur <clement.bonvoisin@ens-cachan.fr>
2014-06-20 14:49:20 +0200

    Organisation problems

--- cusum/offline/offline_efficiency_test.m
+++ cusum/offline/offline_efficiency_test.m
@@ -1,17 +1,19 @@
-function [sumdiff, N, P, diff, np] = offline_efficiency_test(n,p,c,plt)
+function [N, P, diff, sumdiff, np] = offline_efficiency_test(n, p, c, plt)
 
-%[sumdiff, N, P, diff] = offline_efficiency_test(n,p,c,plt) génère un signal aléatoire à n
-%ruptures à partir de (n+1) lois normales centrées, avec p points entre 
-%deux ruptures, où une rupture correspond à une augmentation de c de
-%l'écart-type. On obtient alors une figure représentant en bleu le signal, 
-%en vert les ruptures théoriques, et en rouge les ruptures détectées par
-%l'algorithme CUSUM récursif. En sortie : sumdiff est le RMSD, N est le 
-%nombre de double-ruptures détectées par CUSUM (point de vue offline). Le paramètre plt est
-%booléen et permet de dire si l'on veut ou non visualiser les résultats de
-%segmentation.
+%[N, P, diff, sumdiff, np] = offline_efficiency_test(n,p,c,plt) génère un
+%signal aléatoire à n ruptures à partir de (n+1) lois normales centrées,
+%avec p points entre deux ruptures, où une rupture correspond à une
+%augmentation de c de l'écart-type. On obtient alors une figure
+%représentant en bleu le signal, en vert les ruptures théoriques, et en
+%rouge les ruptures détectées par l'algorithme CUSUM récursif.  Le
+%paramètre plt est booléen et permet de dire si l'on veut ou non visualiser
+%les résultats de segmentation. En sortie : sumdiff est le RMSD, N est le
+%nombre de double-ruptures détectées par CUSUM (point de vue offline), P
+%est le nombre de bonnes ruptures.
 %
-%Deux ruptures sont considérées comme doubles ssi l'écart entre les deux
-%est plus petit que p/100.
+%Deux ruptures détectées sont considérées comme doubles ssi l'écart entre
+%les deux est plus petit que p/5. Une rupture détetée est considérée comme
+%bonne ssi elle se trouve à +/-10% d'une rupture théorique.
 
     Y = zeros((n+1)*p,1);
     nt = zeros(1, n);
