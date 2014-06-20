d98a2128a8098dd1641af42c3d48e1a25e1adf62
chilleur <clement.bonvoisin@ens-cachan.fr>
2014-06-20 14:47:49 +0200

    Minor fixes

--- cusum/offline_efficiency_test.m
+++ cusum/offline_efficiency_test.m
@@ -1,22 +1,23 @@
-function [m, N, P, diff, sumdiff, np] = online_efficiency_test(n, p, c, thresh, plt)
+function [N, P, diff, sumdiff, np] = offline_efficiency_test(n, p, c, plt)
 
-%[m, N, P, diff, sumdiff, np] = online_efficiency_test(n,p,c,thresh,plt)
-%génère un signal à n ruptures à partir de (n+1) lois normales centrées,
+%[N, P, diff, sumdiff, np] = offline_efficiency_test(n,p,c,plt) génère un
+%signal aléatoire à n ruptures à partir de (n+1) lois normales centrées,
 %avec p points entre deux ruptures, où une rupture correspond à une
 %augmentation de c de l'écart-type. On obtient alors une figure
 %représentant en bleu le signal, en vert les ruptures théoriques, et en
-%rouge les ruptures détectées par l'algorithme CUSUM récursif. Le paramètre
-%plt est booléen et permet de dire si l'on veut ou non visualiser les
-%résultats de segmentation. En sortie : sumdiff est le RMSD, N est le
-%nombre de double-ruptures détectées par CUSUM (point de vue online, avec
-%un seuil thresh), P est le nombre de bonnes ruptures, m est le nombre de
-%ruptures.
+%rouge les ruptures détectées par l'algorithme CUSUM récursif.  Le
+%paramètre plt est booléen et permet de dire si l'on veut ou non visualiser
+%les résultats de segmentation. En sortie : sumdiff est le RMSD, N est le
+%nombre de double-ruptures détectées par CUSUM (point de vue offline), P
+%est le nombre de bonnes ruptures.
 %
 %Deux ruptures détectées sont considérées comme doubles ssi l'écart entre
 %les deux est plus petit que p/5. Une rupture détetée est considérée comme
 %bonne ssi elle se trouve à +/-10% d'une rupture théorique.
 
     Y = zeros((n+1)*p,1);
+    nt = zeros(1, n);
+    diff = zeros(1, n);
     N = 0;
     P = 0;
     isokay = false;
@@ -26,11 +27,8 @@ function [m, N, P, diff, sumdiff, np] = online_efficiency_test(n, p, c, thresh, 
         Y((p*(k-1)+1):p*k,1) = (c^k)*randn(p,1);
     end
     
-    times = all_online(Y, {'std'}, thresh);
-    m = size(times,2);
-    np(1, 1:m) = sort(times, 'ascend');
-    nt = floor(np/p)*p;
-    diff = zeros(1, m);
+    times = dikt_cusum_lin(Y, {'std'}, n);
+    np = sort(times, 'ascend');
     
     if (plt == true)
         figure;
@@ -38,7 +36,8 @@ function [m, N, P, diff, sumdiff, np] = online_efficiency_test(n, p, c, thresh, 
         hold on
     end
     
-    for k = 1:m
+    for k = 1:n
+        nt(k) = p*k;
         if (plt == true)
             line([nt(k), nt(k)], [min(Y), max(Y)], 'Color', [0; 0.8; 0]);
             line([np(k), np(k)], [min(Y), max(Y)], 'Color', 'r');     
